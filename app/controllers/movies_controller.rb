82eeea0679cc65613459d6e41b1c02697995843f
Jon Atlas <jjjatlas@hotmail.com>
2016-01-12 16:55:01 -0500

    Add reviews scaffold

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,19 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @movies = Movie.all
   end
 
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
@@ -51,12 +58,11 @@ class MoviesController < ApplicationController
   end
 
   private
-
     def set_movie
       @movie = Movie.find(params[:id])
     end
 
     def movie_params
-      params.require(:movie).permit(:image, :title, :description, :movie_length, :director, :rating, :year)
+      params.require(:movie).permit(:title, :description, :movie_length, :director, :rating, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112170854) do
+ActiveRecord::Schema.define(version: 20160112213655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20160112170854) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
