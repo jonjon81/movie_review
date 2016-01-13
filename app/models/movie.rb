f83d81ea9747ea310c8a3679fc0a8539111b45a7
Jon Atlas <jjjatlas@hotmail.com>
2016-01-13 14:50:20 -0500

    Add validations for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,9 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many  :reviews
 
+  validates :title, :description, :movie_length, :director, :rating, :year, :image_file_name, presence: true
+  validates :movie_length, numericality: {only_integer: true}
+
   has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
